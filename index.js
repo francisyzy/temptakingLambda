217439ec0e3e9e797cbbf1180e735fa8427b748f
BuildTools <unconfigured@null.spigotmc.org>
2020-08-23 00:35:57 +0800

    Fix not running on Lambda

--- index.js
+++ index.js
@@ -16,46 +16,48 @@ let dateAm = String(date_ob.getDate() - 1).padStart(2, '0');
 let month = String(date_ob.getMonth() + 1).padStart(2, '0');
 let year = date_ob.getFullYear();
 
-var options = {
-	method: 'POST',
-	url: 'https://temptaking.herokuapp.com/users/register',
-	// url: 'http://ptsv2.com/t/ob8gg-1598076748/post', //testURL
-	headers: {
-		Authorization:
-			'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJkYXRhIjoxMTYwOTM2NSwiaWF0IjoxNTk2OTkzMTU4LCJleHAiOjE1OTc1OTc5NTh9.nh9-knc59mODdYfq5xt_VmfKBZGbZWXD1a1AP1rzG',
-		'Content-Type': 'application/x-www-form-urlencoded',
-	},
-	form: {
-		PERNR: '11609365',
-		temperature: selectedPmTemp,
-		DateTimePM: year + '-' + month + '-' + datePm + 'T07:' + pmTime + ':20Z',
-		temperaturePM: selectedPmTemp,
-		hasVerified: 'false',
-	},
-};
-request(options, function (error, response) {
-	if (error) throw new Error(error);
-	console.log(response.body);
-});
+exports.handler = function (event, context, callback) {
+	var options = {
+		method: 'POST',
+		url: 'https://temptaking.herokuapp.com/users/register',
+		// url: 'http://ptsv2.com/t/ob8gg-1598076748/post', //testURL
+		headers: {
+			Authorization:
+				'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJkYXRhIjoxMTYwOTM2NSwiaWF0IjoxNTk2OTkzMTU4LCJleHAiOjE1OTc1OTc5NTh9.nh9-knc59mODdYfq5xt_VmfKBZGbZWXD1a1AP1rzG',
+			'Content-Type': 'application/x-www-form-urlencoded',
+		},
+		form: {
+			PERNR: '11609365',
+			temperature: selectedPmTemp,
+			DateTimePM: year + '-' + month + '-' + datePm + 'T07:' + pmTime + ':20Z',
+			temperaturePM: selectedPmTemp,
+			hasVerified: 'false',
+		},
+	};
+	request(options, function (error, response) {
+		if (error) throw new Error(error);
+		console.log(response.body);
+	});
 
-var options2 = {
-	method: 'POST',
-	url: 'https://temptaking.herokuapp.com/users/register',
-	// url: 'http://ptsv2.com/t/ob8gg-1598076748/post', //testURL
-	headers: {
-		Authorization:
-			'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJkYXRhIjoxMTYwOTM2NSwiaWF0IjoxNTk2OTkzMTU4LCJleHAiOjE1OTc1OTc5NTh9.nh9-knc59mODdYfq5xt_VmfKBZGbZWXD1a1AP1rzG',
-		'Content-Type': 'application/x-www-form-urlencoded',
-	},
-	form: {
-		PERNR: '11609365',
-		temperature: selectedAmTemp,
-		DateTimeAM: year + '-' + month + '-' + dateAm + 'T23:' + amTime + ':41.000Z',
-		temperatureAM: selectedAmTemp,
-		hasVerified: 'false',
-	},
+	var options2 = {
+		method: 'POST',
+		url: 'https://temptaking.herokuapp.com/users/register',
+		// url: 'http://ptsv2.com/t/ob8gg-1598076748/post', //testURL
+		headers: {
+			Authorization:
+				'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJkYXRhIjoxMTYwOTM2NSwiaWF0IjoxNTk2OTkzMTU4LCJleHAiOjE1OTc1OTc5NTh9.nh9-knc59mODdYfq5xt_VmfKBZGbZWXD1a1AP1rzG',
+			'Content-Type': 'application/x-www-form-urlencoded',
+		},
+		form: {
+			PERNR: '11609365',
+			temperature: selectedAmTemp,
+			DateTimeAM: year + '-' + month + '-' + dateAm + 'T23:' + amTime + ':41.000Z',
+			temperatureAM: selectedAmTemp,
+			hasVerified: 'false',
+		},
+	};
+	request(options2, function (error, response) {
+		if (error) throw new Error(error);
+		console.log(response.body);
+	});
 };
-request(options2, function (error, response) {
-	if (error) throw new Error(error);
-	console.log(response.body);
-});
